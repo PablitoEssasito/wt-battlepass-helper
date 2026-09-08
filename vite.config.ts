46de05258fcc0d615a5c5a8e089177f929e6a5c1
PablitoEssasito <pawel.adamkiewicz.dev@gmail.com>
2026-09-08 16:59:36 +0200

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/wt-passhelper/",
+  base: "/wt-battlepass-helper/",
   plugins: [react()],
   define: {
     APP_VERSION: JSON.stringify(process.env.npm_package_version)
